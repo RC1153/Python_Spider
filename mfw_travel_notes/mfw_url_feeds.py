9199544f7ec02740f5ce012f8aecc471b851e329
RC1153 <ryan1029@live.cn>
2019-02-23 22:12:40 +0800

    first commit

---
+++ mfw_travel_notes/mfw_url_feeds.py
@@ -0,0 +1,15 @@
+import urllib3
+
+import re
+from bloom_filter import BloomFilter
+import os
+
+request_headers = {
+    'host': "www.mafengwo.cn",
+    'connection': "keep-alive",
+    'cache-control': "no-cache",
+    'upgrade-insecure-requests': "1",
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    'accept-language': "zh-CN,en-US;q=0.8,en;q=0.6"
+}
